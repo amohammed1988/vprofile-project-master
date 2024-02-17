813f46ce0bbf4ada7b0e9b03d6b5d76d196af7a6
aminu mohammed <63984214+amohammed1988@users.noreply.github.com>
2024-02-17 15:17:56 -0600

    Update tomcat.sh

--- vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
+++ vagrant/Automated_provisioning_WinMacIntel/tomcat.sh
@@ -46,8 +46,8 @@ systemctl daemon-reload
 systemctl start tomcat
 systemctl enable tomcat
 
-git clone -b main https://github.com/hkhcoder/vprofile-project.git
-cd vprofile-project
+git clone -b master https://github.com/amohammed1988/vprofile-project-master.git
+cd vprofile-project-master
 mvn install
 systemctl stop tomcat
 sleep 20
